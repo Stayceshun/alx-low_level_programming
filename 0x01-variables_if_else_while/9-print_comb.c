34accfce066ed100456b45054741696e1bc8f0d0
Stacey Eshun <stayceshun@gmail.com>
2022-06-03 21:02:58 -0700

    u

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -19,7 +19,8 @@ int main(void)
 		putchar(' ');
 	}
 
-	putchar('\n');
+	putchar("\n");
 
 	return (0);
 }
+
