512dd62def9e58209c9160e54f4c3fd44d8c438a
Stacey Eshun <stayceshun@gmail.com>
2022-06-13 15:31:48 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@ void print_rev(char *s)
 {
 	int len = 0, i = 0;
 
-	while (s[i++]
-			len++;
+	while (s[i++])
+		len++;
 
-			for (i = len - 1; i >= 0; i--)
-			_putchar(s[i]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-			_putchar('\n');
-			}
+	_putchar('\n');
+}
 
